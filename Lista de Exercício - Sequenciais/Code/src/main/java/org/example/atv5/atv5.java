41f2e2400d9fb49f3b09832f78c7df670fb18df3
Eduardo <eduardo.a.junior.dev@gmail.com>
2024-05-02 22:56:15 -0300

    exercicio 5 finalizado

---
+++ Lista de Exercício - Sequenciais/Code/src/main/java/org/example/atv5/atv5.java
@@ -0,0 +1,36 @@
+package org.example.atv5;
+
+/*
+* Faça um algoritmo que leia 3 valores a, b e c, coeficientes de uma equação de segundo
+grau, e calcule e escreva as raizes da equação. Dica: As raízes de uma equação podem
+ser calculadas pela fórmula de Baskhara.
+* */
+
+import java.util.Scanner;
+
+public class atv5 {
+
+    public static void main(String[] args) {
+
+        double a, b, c, delta, raiz1, raiz2;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o valor de a: ");
+        a = sc.nextDouble();
+        System.out.println("Digite o valor de b: ");
+        b = sc.nextDouble();
+        System.out.println("Digite o valor de c: ");
+        c = sc.nextDouble();
+
+        delta = b * b - 4 * a * c;
+
+        raiz1 = (-b + Math.sqrt(delta)) / (2 * a);
+        raiz2 = (-b - Math.sqrt(delta)) / (2 * a);
+
+        System.out.println("Raiz 1: " + raiz1);
+        System.out.println("Raiz 2: " + raiz2);
+
+        sc.close();
+    }
+}
