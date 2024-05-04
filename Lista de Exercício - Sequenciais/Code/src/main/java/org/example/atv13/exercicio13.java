19f0467fdcc261b97f38531cdd0dd60e5263ad59
Eduardo <eduardo.a.junior.dev@gmail.com>
2024-05-03 23:02:53 -0300

    exercicio 13 finalizado

---
+++ Lista de Exercício - Sequenciais/Code/src/main/java/org/example/atv13/exercicio13.java
@@ -0,0 +1,29 @@
+package org.example.atv13;
+
+/*Faça um algoritmo que leia a quantidade de alunos em uma sala de aula e a
+quantidade de alunos por grupo, e calcule e escreva quantos grupos serão formados e
+o resto de alunos que não foram suficientes para formar mais um grupo.
+*/
+
+import java.util.Scanner;
+
+public class exercicio13 {
+
+    public static void main(String[] args) {
+
+        int quantAlunosSala, quantAlunosGrupo, restoGrupo, divisãoGrupo;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantidade de alunos sala: ");
+        quantAlunosSala = sc.nextInt();
+        System.out.print("Quantidade de alunos grupo: ");
+        quantAlunosGrupo = sc.nextInt();
+
+        divisãoGrupo = quantAlunosSala / quantAlunosGrupo;
+        restoGrupo = quantAlunosSala % quantAlunosGrupo;
+
+        System.out.println("Quantidade de grupo: " + divisãoGrupo);
+        System.out.println("Quantidade de alunos restantes: " + restoGrupo);
+    }
+}
